2298ac2a4331c3f1f6904db571b3d159b167214a
Rohit Jain <rohit.jain@treebohotels.com>
2020-06-03 13:48:21 +0530

    Read db creds in multiple ways. Fix downgrade command to only rollback last applied migration.

--- dbmigrate/cli.py
+++ dbmigrate/cli.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
+import json
 import os
-import sys
 
 import click
-import psycopg2
 
 from dbmigrate.database import Database
 from dbmigrate.migration_directory import MigrationDirectory
@@ -37,26 +36,28 @@ develop branch, before generating migration. Or even better would be to generate
 """
 
 
-def read_all_tenant_db_creds():
-    tenant_db_creds = []
+def read_all_db_creds():
+    all_db_creds = []
 
-    conn = psycopg2.connect(dsn=os.environ.get("DATABASE_URL"))
-    try:
-        with conn:
-            with conn.cursor() as cursor:
-                # TODO: Provide a way to inject tenant_database credentials from other sources
-                cursor.execute(
-                    "SELECT db_user, db_password, db_host, db_port, db_name, db_schema FROM tenant_db_config WHERE "
-                    "user_type='admin';")
-                all_tenant_db_creds = cursor.fetchall()
-                for db_creds in all_tenant_db_creds:
-                    tenant_db_creds.append(
-                        dict(db_user=db_creds[0], db_password=db_creds[1], db_host=db_creds[2], db_port=db_creds[3],
-                             db_name=db_creds[4], db_schema=db_creds[5]))
-    finally:
-        conn.close()
+    if os.environ.get("DATABASE_URI"):
+        all_db_creds.append(
+            dict(database_uri=os.environ.get("DATABASE_URI"), db_schema=os.environ.get("SCHEMA", 'public')))
 
-    return tenant_db_creds
+    elif os.environ.get("DB_USER"):
+        all_db_creds.append(dict(db_user=os.environ.get("DB_USER"), db_password=os.environ.get("DB_PASSWORD"),
+                                 db_host=os.environ.get("DB_HOST"), db_port=os.environ.get("DB_PORT", 5432),
+                                 db_name=os.environ.get("DB_NAME"), db_schema=os.environ.get("SCHEMA", 'public')))
+
+    else:
+        # Supports migrations on multiple databases
+        with open("db_creds.json", 'r') as db_creds:
+            database_creds = json.load(db_creds)
+            for db_creds in database_creds:
+                all_db_creds.append(dict(db_user=db_creds['db_user'], db_password=db_creds['db_password'],
+                                         db_host=db_creds['db_host'], db_port=db_creds.get('db_port', 5432),
+                                         db_name=db_creds['db_name'], db_schema=db_creds.get('db_schema', 'public')))
+
+    return all_db_creds
 
 
 @click.group()
@@ -81,16 +82,27 @@ def touch(name):
 @main.command()
 def init():
     """Initializes migration table on database"""
-    Database.init_migration(tenant_db_creds=read_all_tenant_db_creds())
+    Database.init_migration(db_creds=read_all_db_creds())
 
 
 @main.command()
 def upgrade():
     """Apply remaining un-applied forward migrations"""
-    Database.run_migrations('upgrade', tenant_db_creds=read_all_tenant_db_creds())
+    Database.run_migrations('upgrade', db_creds=read_all_db_creds())
 
 
 @main.command()
 def downgrade():
     """Rollbacks the last applied migrations"""
-    Database.run_migrations('downgrade', tenant_db_creds=read_all_tenant_db_creds())
+    Database.run_migrations('downgrade', db_creds=read_all_db_creds())
+
+
+@main.command()
+def dump():
+    db_creds = read_all_db_creds()[0]
+    db_schema = db_creds['db_schema']
+    if 'database_uri' in db_creds:
+        database_uri = db_creds['database_uri']
+    else:
+        database_uri = "postgres://%(db_user)s:%(db_password)s@%(db_host)s:%(db_port)s/%(db_name)s" % db_creds
+    Database.dump(database_uri, db_schema)
--- dbmigrate/database.py
+++ dbmigrate/database.py
@@ -1,4 +1,5 @@
 import datetime
+import subprocess
 
 import psycopg2
 
@@ -16,14 +17,15 @@ class Database(object):
         return conn
 
     @classmethod
-    def init_migration(cls, tenant_db_creds):
+    def init_migration(cls, db_creds):
         CREATE_MIGRATIONS_TABLE_QUERY = """CREATE TABLE IF NOT EXISTS migrations (
         version character varying not null,
-        applied_at timestamp with time zone not null);"""
+        applied_at timestamp with time zone not null
+        rollbacked boolean);"""
 
-        for tenant_db_cred in tenant_db_creds:
-            database_uri = "postgres://%(db_user)s:%(db_password)s@%(db_host)s:%(db_port)s/%(db_name)s" % tenant_db_cred
-            schema = tenant_db_cred.get('db_schema')
+        for db_cred in db_creds:
+            database_uri = "postgres://%(db_user)s:%(db_password)s@%(db_host)s:%(db_port)s/%(db_name)s" % db_cred
+            schema = db_cred.get('db_schema')
 
             print("Running init_migration on database: %s, and schema: %s" % (database_uri, schema))
 
@@ -37,50 +39,105 @@ class Database(object):
 
     @classmethod
     def fetch_last_applied_migration(cls, database_uri, schema):
-        FETCH_LAST_MIGRATION_QUERY = "SELECT version FROM migrations ORDER BY applied_at DESC LIMIT 1;"
+        FETCH_LAST_MIGRATION_QUERY = "SELECT version FROM migrations WHERE rollbacked = %s ORDER BY applied_at " \
+                                     "DESC LIMIT 1;"
 
         conn = cls.new_connection(database_uri, schema)
         try:
             with conn:
                 with conn.cursor() as cursor:
-                    cursor.execute(FETCH_LAST_MIGRATION_QUERY)
+                    cursor.execute(FETCH_LAST_MIGRATION_QUERY, (False,))
                     last_version = cursor.fetchone()
         finally:
             conn.close()
         return last_version[0] if last_version else None
 
     @classmethod
-    def run_migrations(cls, migration_direction, tenant_db_creds):
-        for tenant_db_cred in tenant_db_creds:
-            database_uri = "postgres://%(db_user)s:%(db_password)s@%(db_host)s:%(db_port)s/%(db_name)s" % tenant_db_cred
-            schema = tenant_db_cred.get('db_schema')
+    def run_migrations(cls, migration_direction, db_creds):
+        for db_cred in db_creds:
+            database_uri = "postgres://%(db_user)s:%(db_password)s@%(db_host)s:%(db_port)s/%(db_name)s" % db_cred
+            schema = db_cred.get('db_schema')
 
-            last_version = cls.fetch_last_applied_migration(database_uri, schema)
-            graph = MigrationDirectory.prepare_migration_graph_to_apply(last_version)
-            if not graph:
-                print("Nothing to migrate")
-                continue
+            if migration_direction == 'upgrade':
+                cls.upgrade(database_uri, schema)
 
-            print("Running migration on database: %s, and schema: %s" % (database_uri, schema))
+            elif migration_direction == 'downgrade':
+                cls.downgrade(database_uri, schema)
 
-            conn = cls.new_connection(database_uri, schema)
+            else:
+                raise Exception("Invalid migration direction")
 
-            try:
-                for revision in graph:
-                    with conn:
-                        # Apply Migrations
-                        with conn.cursor() as cursor:
-                            revision_file = MigrationDirectory.get_migration_file_path(revision)
-                            sql_commands = MigrationFile.parse_file_and_read_migration_commands(revision_file,
-                                                                                                migration_direction)
-                            if not sql_commands:
-                                continue
+    @classmethod
+    def upgrade(cls, database_uri, schema):
+        last_version = cls.fetch_last_applied_migration(database_uri, schema)
 
-                            print("Running upgrade: {0} -> {1}".format(last_version, revision))
-                            cursor.execute(sql_commands)
-                            cursor.execute("INSERT INTO migrations (version, applied_at) VALUES (%s, %s)",
-                                           (revision, datetime.datetime.now()))
-                            last_version = revision
+        graph = MigrationDirectory.prepare_migration_graph_to_apply(last_version)
+        if not graph:
+            print("Nothing to migrate")
+            return
 
-            finally:
-                conn.close()
+        print("Running migration on database: %s, and schema: %s" % (database_uri, schema))
+
+        conn = cls.new_connection(database_uri, schema)
+
+        try:
+            for revision in graph:
+                with conn:
+                    # Apply Migrations
+                    with conn.cursor() as cursor:
+                        revision_file = MigrationDirectory.get_migration_file_path(revision)
+                        sql_commands = MigrationFile.parse_file_and_read_migration_commands(revision_file,
+                                                                                            'upgrade')
+                        if not sql_commands:
+                            continue
+
+                        print("Running upgrade: {0} -> {1}".format(last_version, revision))
+                        cursor.execute(sql_commands)
+                        cursor.execute("INSERT INTO migrations (version, applied_at) VALUES (%s, %s)",
+                                       (revision, datetime.datetime.now()))
+                        last_version = revision
+
+        finally:
+            conn.close()
+
+    @classmethod
+    def downgrade(cls, database_uri, schema):
+        last_version = cls.fetch_last_applied_migration(database_uri, schema)
+        if not last_version:
+            print("Nothing to rollback")
+            return
+
+        print("Rollbacking last applied migration version: %s, on database: %s, and "
+              "schema: %s" % (last_version, database_uri, schema))
+
+        revision_file = MigrationDirectory.get_migration_file_path(last_version)
+        sql_commands = MigrationFile.parse_file_and_read_migration_commands(revision_file,
+                                                                            'downgrade')
+        if not sql_commands:
+            print("Can't find SQL commands to rollback this migration")
+            return
+
+        conn = cls.new_connection(database_uri, schema)
+        try:
+            with conn:
+                with conn.cursor() as cursor:
+                    print("Running downgrade: {0}".format(last_version))
+                    cursor.execute(sql_commands)
+                    cursor.execute("UPDATE migrations SET rollbacked = %s WHERE version = %s", (True, last_version))
+        finally:
+            conn.close()
+
+    @staticmethod
+    def dump(database_uri, db_schema):
+        schema_file_path = MigrationDirectory.get_migration_file_path('schema')
+
+        process = subprocess.Popen(
+            ["pg_dump", "--format=plain", "--encoding=UTF8", "--schema-only", "--no-privileges", "--no-owner",
+             "-n", db_schema, "-f", schema_file_path, database_uri],
+            stdout=subprocess.PIPE, universal_newlines=True)
+
+        output = process.communicate()[0]
+        if int(process.returncode) != 0:
+            print('Command failed. Return code : {}'.format(process.returncode))
+            exit(1)
+        return output
